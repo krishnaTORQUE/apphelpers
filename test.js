c4e5a031addbe7be836ce14eab37d812cb7dfce1
Krishna Torque <>
2021-02-26 00:30:04 +0530

    Test matchEmail Function

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
-const { ucwords } = require('./index');
+const { ucwords, matchEmail } = require('./index');
 
 (async () => {
   const str = await ucwords('hello world');
   console.log(str);
+  // return Hello World
+
+  const content = 'Hello sir / mam "name@email.com" How are you?';
+  console.log(await matchEmail(content));
+  // return [ 'name@email.com' ]
 })();
